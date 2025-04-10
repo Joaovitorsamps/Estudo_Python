5bf1721b20d88ca4b2edf2f101477a0125d18702
João Sampaio <joaovitorsamps@hotmil.com>
2025-04-10 12:56:42 -0400

    Introduzindo o uso de listas em Python

---
+++ Estrutura_de_Dados/Listas/Consultando_Indices.py
@@ -0,0 +1,5 @@
+frutas = ["laranja", "maçã", "pêra", "uva"]
+#             0         1       2      3
+#            -4        -3      -2     -1
+frutas [0] #laranja
+frutas [-1] #uva
---
+++ Estrutura_de_Dados/Listas/Declarando_Listas.py
@@ -0,0 +1,16 @@
+#Declarando Listas
+
+#Declarando listas com valores
+frutas = ["laranja", "maçã", "uva"]
+             #0         1      2
+#Declarando uma lista vazia
+frutas = []
+
+#Declarando uma lista com o construtto "list" onde cada letra é um elemnto
+letras = list("python")
+
+#Gera elementos de 0 a 9 (10 elementos)
+numeros = list(range(10))
+
+#Declarando atributos
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "Manaus", True]
